1109626683897362401f337a81786563b2f10816
James1412 <73318218+James1412@users.noreply.github.com>
2023-11-28 12:53:59 -0500

    dispose controller

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "TikTok Clone",
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
