dd3a1b6d3b3457d676d7fcec5731c98ba7606414
James1412 <73318218+James1412@users.noreply.github.com>
2023-12-04 16:45:43 -0500

    tabpageselector

--- lib/features/onboarding/tutorial_screen.dart
+++ lib/features/onboarding/tutorial_screen.dart
@@ -12,90 +12,109 @@ class TutorialScreen extends StatefulWidget {
 class _TutorialScreenState extends State<TutorialScreen> {
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
+    return DefaultTabController(
       length: 3,
       child: Scaffold(
-        body: SafeArea(
-          child: TabBarView(children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gaps.v32,
-                  Text(
-                    "Watch cool videos!",
-                    style: TextStyle(
-                      fontSize: Sizes.size36,
-                      height: 1.2,
-                      fontWeight: FontWeight.bold,
+        body: const SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v32,
+                    Text(
+                      "Watch cool videos!",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        height: 1.2,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  Gaps.v20,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
+                    Gaps.v20,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v32,
+                    Text(
+                      "Follow the steps",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        height: 1.2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v20,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Sizes.size20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Gaps.v32,
+                    Text(
+                      "Enjoy the ride",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        height: 1.2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v20,
+                    Text(
+                      "Videos are personalized for you based on what you watch, like, and share.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          surfaceTintColor: Colors.white,
+          color: Colors.white,
+          child: Container(
+            decoration: const BoxDecoration(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TabPageSelector(
+                  selectedColor: Theme.of(context).primaryColor,
+                  color: const Color(0xFF4BE2ED),
+                  borderStyle: BorderStyle.none,
+                ),
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gaps.v32,
-                  Text(
-                    "Follow the steps",
-                    style: TextStyle(
-                      fontSize: Sizes.size36,
-                      height: 1.2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Gaps.v20,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: Sizes.size20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Gaps.v32,
-                  Text(
-                    "Enjoy the ride",
-                    style: TextStyle(
-                      fontSize: Sizes.size36,
-                      height: 1.2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Gaps.v20,
-                  Text(
-                    "Videos are personalized for you based on what you watch, like, and share.",
-                    style: TextStyle(
-                      fontSize: Sizes.size20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ]),
+          ),
         ),
       ),
     );
