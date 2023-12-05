8944070ce8034dc2b4e1e78fca361035e4d66b04
James1412 <73318218+James1412@users.noreply.github.com>
2023-12-05 13:24:24 -0500

    pause button

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -48,7 +48,7 @@ class _VideoTimeLineScreenState extends State<VideoTimeLineScreen> {
       scrollDirection: Axis.vertical,
       itemCount: _itemCount,
       itemBuilder: (context, index) {
-        return VideoPost(onVideoFinished: _onVideoFinished);
+        return VideoPost(onVideoFinished: _onVideoFinished, index: index);
       },
     );
   }
