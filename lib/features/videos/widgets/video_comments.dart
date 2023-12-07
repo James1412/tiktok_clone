7ad215f7064f37ae934a98820ce46d2993d0f27d
James1412 <73318218+James1412@users.noreply.github.com>
2023-12-07 13:14:32 -0500

    comment section to stack

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -38,75 +38,87 @@ class _VideoCommentsTabState extends State<VideoCommentsTab> {
             ),
           ],
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.symmetric(
-            vertical: Sizes.size10,
-            horizontal: Sizes.size16,
-          ),
-          itemCount: 10,
-          separatorBuilder: (context, index) => Gaps.v20,
-          itemBuilder: (context, index) {
-            return Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CircleAvatar(
-                  radius: 18,
-                  child: Text("지강"),
-                ),
-                Gaps.h10,
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "지강",
-                        style: TextStyle(
-                          color: Colors.grey.shade500,
-                          fontWeight: FontWeight.bold,
-                          fontSize: Sizes.size12,
-                        ),
-                      ),
-                      Gaps.v2,
-                      const Text(
-                          "That's not it. Ive seen the same thing, but also in a cave"),
-                    ],
-                  ),
-                ),
-                Gaps.h10,
-                Column(
+        body: Stack(
+          children: [
+            ListView.separated(
+              padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size10,
+                horizontal: Sizes.size16,
+              ),
+              itemCount: 10,
+              separatorBuilder: (context, index) => Gaps.v20,
+              itemBuilder: (context, index) {
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    FaIcon(
-                      FontAwesomeIcons.heart,
-                      size: Sizes.size20,
-                      color: Colors.grey.shade500,
+                    const CircleAvatar(
+                      radius: 18,
+                      child: Text("지강"),
                     ),
-                    Gaps.v2,
-                    Text(
-                      "52.2K",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                        fontSize: Sizes.size12,
+                    Gaps.h10,
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "지강",
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                              fontWeight: FontWeight.bold,
+                              fontSize: Sizes.size12,
+                            ),
+                          ),
+                          Gaps.v2,
+                          const Text(
+                              "That's not it. Ive seen the same thing, but also in a cave"),
+                        ],
                       ),
+                    ),
+                    Gaps.h10,
+                    Column(
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size20,
+                          color: Colors.grey.shade500,
+                        ),
+                        Gaps.v2,
+                        Text(
+                          "52.2K",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontSize: Sizes.size12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            Positioned(
+              width: MediaQuery.of(context).size.width,
+              bottom: 0,
+              child: BottomAppBar(
+                color: Colors.white,
+                surfaceTintColor: Colors.white,
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Colors.grey.shade500,
+                      foregroundColor: Colors.white,
+                      child: const Text("지강"),
+                    ),
+                    Gaps.h10,
+                    const Expanded(
+                      child: TextField(),
                     ),
                   ],
                 ),
-              ],
-            );
-          },
-        ),
-        bottomNavigationBar: BottomAppBar(
-          color: Colors.white,
-          surfaceTintColor: Colors.white,
-          child: Row(
-            children: [
-              CircleAvatar(
-                radius: 18,
-                backgroundColor: Colors.grey.shade500,
-                foregroundColor: Colors.white,
-                child: const Text("지강"),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
