1fa806c862bd5776cf5cd9b6b42ad40587d5061d
James1412 <73318218+James1412@users.noreply.github.com>
2023-12-12 03:32:13 -0500

    settings screen with showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -17,8 +17,58 @@ class SettingsScreen extends StatelessWidget {
         ),
       ),
       body: ListView(
-        children: const [
-          AboutListTile(),
+        children: [
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: Theme.of(context).copyWith(
+                      colorScheme: const ColorScheme.light(
+                        primary: Colors.white, // header background color
+                        onPrimary: Colors.black, // header text color
+                        onSurface: Colors.black, // body text color
+                      ),
+                      textButtonTheme: TextButtonThemeData(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.red, // button text color
+                        ),
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(date);
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
